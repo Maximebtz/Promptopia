61eb08250e1ca9ee9be857bbdadc0e19752f8530
Maximebtz <maxime_bertuzzi@hotmail.com>
2024-07-02 12:55:58 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["mongoose"],
-    },
-    images: {
-      domains: ['lh3.googleusercontent.com'],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      }
-      return config
+  experimental: {
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
     }
-  };
-  
-  export default nextConfig;
-  
+    return config
+  }
+};
+
+export default nextConfig;
